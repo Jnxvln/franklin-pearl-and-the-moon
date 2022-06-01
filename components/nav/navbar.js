3df126113f407dca9c2532980b71b5a3fb31b78e
Justin Cox <justincox@Violmusio.attlocal.net>
2022-06-01 07:07:09 -0500

    Update navbar to remove extraneous markup and styles

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -5,15 +5,11 @@ const navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles['nav-list']}>
-        <div className={styles['nav-list-item']}>
-          <Link href="/" style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
-        </div>
-        <div className={styles['nav-list-item']}>Music</div>
-        <div className={styles['nav-list-item']}>Shows</div>
-        <div className={styles['nav-list-item']}>Merch</div>
-        <div onClick={() => {
-          window.scrollTo(0, document.body.scrollHeight)
-        }} className={styles['nav-list-item']}>Contact</div>
+          <Link href="/">Home</Link>
+          <Link href="/music">Music</Link>
+          <Link href="/shows">Shows</Link>
+          <Link href="/merch">Merch</Link>
+          <Link href="/contact">Contact</Link>
       </div>
     </nav>
   )
